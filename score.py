05c559ce3ac153a0ac7d2fad6f9123380646410b
codemmando <rene.mikkelsen@protonmail.com>
2025-01-30 13:37:36 +0100

    started score class, updated readme

---
+++ score.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Score:
+
+    def __init__(self, x, y):
+        self.score = 0
+        self.pos_x = x
+        self.pos_y = y
+
+    def draw(self, screen):
+        pygame.draw    
+        #https://www.pygame.org/docs/ref/font.html#pygame.font.Font.render
